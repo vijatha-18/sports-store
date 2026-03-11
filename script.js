059ed6c6f1f17eabd7ced9cef17e7a46bf867b55
vijatha-18 <Kvijetha50@gmail.com>
2026-03-11 19:26:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ updateCart()
 function addToCart(name,price){
 
 if(cart[name]){
-if(cart[name].qty < 8) cart[name].qty++
+
+if(cart[name].qty >= 10){
+alert("Out of Stock ❌")
+return
+}
+
+cart[name].qty++
+
 }else{
+
 cart[name]={price:price,qty:1}
+
 }
 
 localStorage.setItem("cart",JSON.stringify(cart))
@@ -30,7 +39,7 @@ for(let item in cart){
 
 let li=document.createElement("li")
 
-li.innerHTML=item+" x"+cart[item].qty+" - ₹"+cart[item].price*cart[item].qty
+li.innerText=item+" x"+cart[item].qty+" - ₹"+cart[item].price*cart[item].qty
 
 cartItems.appendChild(li)
 
@@ -63,14 +72,22 @@ popup.style.display="none"
 document.getElementById("search").addEventListener("keyup",function(){
 
 let filter=this.value.toLowerCase()
+let found=false
 
 document.querySelectorAll(".product").forEach(p=>{
 
 let name=p.querySelector("h3").innerText.toLowerCase()
 
-p.style.display=name.includes(filter)?"block":"none"
+if(name.includes(filter)){
+p.style.display="block"
+found=true
+}else{
+p.style.display="none"
+}
 
 })
+
+document.getElementById("no-products").style.display=found?"none":"block"
 
 })
 
@@ -78,7 +95,7 @@ function filterCategory(category){
 
 document.querySelectorAll(".product").forEach(p=>{
 
-if(category=="all" || p.classList.contains(category)){
+if(category==="all" || p.classList.contains(category)){
 p.style.display="block"
 }else{
 p.style.display="none"
@@ -91,7 +108,6 @@ p.style.display="none"
 document.getElementById("sort").addEventListener("change",function(){
 
 let products=[...document.querySelectorAll(".product")]
-
 let container=document.getElementById("product-list")
 
 products.sort((a,b)=>{
@@ -99,10 +115,33 @@ products.sort((a,b)=>{
 let pa=a.dataset.price
 let pb=b.dataset.price
 
-return this.value=="low"?pa-pb:pb-pa
+return this.value==="low"?pa-pb:pb-pa
 
 })
 
 products.forEach(p=>container.appendChild(p))
 
 })
+
+const toggle=document.getElementById("themeToggle")
+
+let theme=localStorage.getItem("theme")
+
+if(theme==="light"){
+document.body.classList.add("light")
+toggle.innerText="☀️"
+}
+
+toggle.onclick=()=>{
+
+document.body.classList.toggle("light")
+
+if(document.body.classList.contains("light")){
+localStorage.setItem("theme","light")
+toggle.innerText="☀️"
+}else{
+localStorage.setItem("theme","dark")
+toggle.innerText="🌙"
+}
+
+}
